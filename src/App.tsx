067f1a02809722d1998a7ce7d2bd9ac6fe846f13
Nitin Kushwaha <72150554+gigglernitin@users.noreply.github.com>
2024-03-22 23:11:00 +0530

    Update PDF page number in App component

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ import resume from "./assets/CSE493_QUIZ 1 _ Quizizz.pdf";
 function MyApp() {
   return (
     <div>
-      <PDF file={resume} onDocumentComplete={numPages => console.log(numPages)} scale={1} page={5} />
+      <PDF file={resume} onDocumentComplete={numPages => console.log(numPages)} scale={1} page={1} />
     </div>
   );
 }
